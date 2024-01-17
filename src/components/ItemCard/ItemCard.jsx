77dcef66ab96a8a901eb936607198af364845287
sofibaserga <sofi.baserga@gmail.com>
2024-01-17 19:42:38 -0300

    sigo arreglando errores

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,6 +1,6 @@
 
 import { Link } from "react-router-dom";
-import Boton from "../Boton/boton";
+import Boton from "../Boton/Boton";
 
 
 const ItemCard = ({item}) => {
@@ -8,7 +8,7 @@ const ItemCard = ({item}) => {
     return (
       <Link to={`/item/${item.id}`} className="text-center flex flex-col items-center justify-end	pb-10">
         <img src={item.portada} alt={item.name} className="rounded-3xl mb-8 w-80" />
-        <h3 className="text-lg mb-1 font-medium">{item.name}</h3>
+        <h3 className="text-lg mb-1 font-medinoum">{item.name}</h3>
         <p className="w-80 mb-4">{item.description}</p>
         <p className="text-lg text-verdeClaroTognis ">${item.price}</p>
         <Boton>Ver más</Boton>
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -6,7 +6,7 @@ import ItemList from "../ItemList/ItemList"
 import { useParams } from "react-router-dom"
 import { collection, getDocs, query, where } from "firebase/firestore"
 import { db } from "../../firebase/config"
-import Contenedor from "../Contenedor.jsx/Contenedor"
+import Contenedor from "../Contenedor/Contenedor"
 
 
 const ItemListContainer = ({ greeting }) => {
